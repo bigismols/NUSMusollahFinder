24dd039774140b1adf64e2d38702d877156a1eab
bigismols <bigismols239@gmail.com>
2024-06-22 17:15:10 +0800

    Login Screen and Signup Screen created

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,15 +1,19 @@
 import { Image, View, TextInput, TouchableOpacity, Text, ImageBackground } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import LoginScreen from './LoginScreen';
 
-const LoginScreen = () => {
-  const navigation = useNavigation();
+const SignupScreen = () => {
+    const navigation=useNavigation();
   return (
     <View className='flex-1'> 
       <ImageBackground source={require('../assets/images/bg-green.jpg')} resizeMode='cover'
       className='w-full h-full absolute'/>
       <View className='flex space-y-4 mx-4 items-center'>
         <View className='w-full mt-80 rounded-2xl items-center bg-gray-200'>
+          <TextInput placeholder='Username' placeholderTextColor={'gray'} />
+        </View>
+        <View className='w-full rounded-2xl items-center bg-gray-200'>
           <TextInput placeholder='Email' placeholderTextColor={'gray'} />
         </View>
         <View className='w-full rounded-2xl items-center bg-gray-200'>
@@ -19,9 +23,9 @@ const LoginScreen = () => {
             <Text>Login</Text>
         </TouchableOpacity>
         <View className='flex-row'>
-          <Text>Don't have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.push('SignupScreen')}>
-            <Text className='text-blue-500 underline'> Sign up</Text>
+          <Text>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.push('LoginScreen')}>
+            <Text className='text-blue-500 underline'> Log in</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -29,4 +33,4 @@ const LoginScreen = () => {
   )
 }
 
-export default LoginScreen
+export default SignupScreen
