6e21ba401dfc26a739cdbecc006fb91f17c7a450
bigismols <bigismols239@gmail.com>
2024-06-22 16:57:02 +0800

    Added LoginScreen to Navigation

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
@@ -17,11 +15,10 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-            <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
-              <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{headerShown: false}}>
               <Stack.Screen name='Home' component={HomeScreen} />
-              <Stack.Screen name='MapScreen' component={MapScreen} 
-              options={headerShown=false}/>
+              <Stack.Screen name='MapScreen' component={MapScreen} />
+              <Stack.Screen name='LoginScreen' component={LoginScreen} />
             </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
