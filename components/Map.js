42051b117b720bf7fc5087273fe044c718d78a8e
Adib <bigismols239@gmail.com>
2024-06-30 21:48:28 +0800

    Input Musollah location into DB and on map

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,18 @@
 import { StyleSheet } from 'react-native'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectDestination, selectOrigin, setOrigin } from '../slices/navSlice'
 import * as Location from 'expo-location'
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore"; 
+import { db } from '../firebase'
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
   const dispatch = useDispatch();
   const mapRef = useRef(null);
+  const [musollahs, setMusollahs] = useState([]);
 
   useEffect(() => {
       const getPermissions = async () => {
@@ -36,13 +39,31 @@ const Map = () => {
   useEffect(() => {
     if (mapRef.current && origin?.location) {
       mapRef.current.animateToRegion({
-        latitude: origin.location.latitude,
-        longitude: origin.location.longitude,
+        // latitude: origin.location.latitude,
+        // longitude: origin.location.longitude,
+        latitude: 1.2968749382461264, 
+        longitude: 103.77642571509065,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       });
     }
   }, [origin]);
+
+  useEffect(() => {
+    const fetchMusollahs = async () => {
+      try {
+        const colRef = collection(db, 'musollahs');
+        const snapshot = await getDocs(colRef);
+        const data = snapshot.docs.map(doc => doc.data());
+        console.log(data);
+        setMusollahs(data);
+      } catch (error) {
+        console.error('Error fetching musollahs:', error);
+      }
+    };
+
+    fetchMusollahs();
+  }, []);
 
   return (
     <MapView style={styles.map}
@@ -72,7 +93,8 @@ const Map = () => {
 
       {destination?.location && (
         <Marker
-          coordinate={{
+          coordinate={
+            {
               latitude: destination.location.latitude,
               longitude: destination.location.longitude,
             }
@@ -81,6 +103,19 @@ const Map = () => {
           identifier='Destination'
         />
       )}
+
+      {musollahs.map((musollah) => (
+        <Marker
+          key={musollah.name}
+          coordinate={
+            {
+              latitude: musollah.latitude,
+              longitude: musollah.longitude
+            }
+          }
+          title={musollah.name}
+        />
+      ))}
     </MapView>
   )
 }
--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,15 +18,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig);
-} else {
-    app = firebase.app();
-}
-
-const auth = firebase.auth();
-
-export { auth };
-
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
