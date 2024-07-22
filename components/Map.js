1b2380bbb8400f1bae7da21db97e22146a8a3d4f
Adib <bigismols239@gmail.com>
2024-07-22 19:20:33 +0800

    Added custom markers and changed callouts

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,16 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
-import MapView, { Callout, Marker } from 'react-native-maps'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectDestination, selectOrigin, setOrigin } from '../slices/navSlice'
-import * as Location from 'expo-location'
-import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore"; 
-import { FIRESTORE_DB } from '../FirebaseConfig'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { useSelector, useDispatch } from "react-redux";
+import { selectDestination, selectOrigin, setOrigin } from "../slices/navSlice";
+import * as Location from "expo-location";
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import { FIRESTORE_DB } from "../FirebaseConfig";
 // import {Svg, Image as ImageSvg} from 'react-native-svg'
-import { GOOGLE_MAPS_APIKEY } from '@env';
-import MapViewDirections from 'react-native-maps-directions'
-import { useNavigation } from '@react-navigation/native'
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import MapViewDirections from "react-native-maps-directions";
+import { useNavigation } from "@react-navigation/native";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 // hello
 const Map = () => {
   const origin = useSelector(selectOrigin);
@@ -18,81 +19,79 @@ const Map = () => {
   const mapRef = useRef(null);
   const [musollahs, setMusollahs] = useState([]);
   const navigation = useNavigation();
-  
 
   useEffect(() => {
-      const getPermissions = async () => {
-        let {status} = await Location.requestForegroundPermissionsAsync();
-        let {backStatus} = await Location.requestBackgroundPermissionsAsync();
-        if (status !== 'granted' && backStatus !== 'granted') {
-          console.log('Please grant location permissions!')
-          return;
-        } 
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      let { backStatus } = await Location.requestBackgroundPermissionsAsync();
+      if (status !== "granted" && backStatus !== "granted") {
+        console.log("Please grant location permissions!");
+        return;
+      }
 
-        let currentLocation = await Location.getCurrentPositionAsync();
-        // console.log(currentLocation);
-        dispatch(setOrigin({
+      let currentLocation = await Location.getCurrentPositionAsync();
+      // console.log(currentLocation);
+      dispatch(
+        setOrigin({
           location: {
             // latitude: currentLocation.coords.latitude,
             // longitude: currentLocation.coords.longitude,
             latitude: 1.2968749382461264,
             longitude: 103.77642571509065,
           },
-        }))
-      }
-      getPermissions();
-    }, [] 
-  )
+        })
+      );
+    };
+    getPermissions();
+  }, []);
 
   // useEffect(() => {
   //   if (mapRef.current && origin?.location) {
   //     mapRef.current.animateToRegion({
   //       // latitude: origin.location.latitude,
   //       // longitude: origin.location.longitude,
-        // latitude: 1.2968749382461264, 
-        // longitude: 103.77642571509065,
+  // latitude: 1.2968749382461264,
+  // longitude: 103.77642571509065,
   //       latitudeDelta: 0.005,
   //       longitudeDelta: 0.005,
   //     });
   //   }
   // }, [origin]);
 
-  useEffect(
-    () => 
-  //     {
-  //   if (mapRef.current && destination?.location) {
-  //     mapRef.current.animateToRegion({
-  //       // latitude: origin.location.latitude,
-  //       // longitude: origin.location.longitude,
-  //       latitude: destination.location.latitude, 
-  //       longitude: destination.location.longitude,
-  //       latitudeDelta: 0.005,
-  //       longitudeDelta: 0.005,
-  //     });
-  //   }
-  // }, [destination]
-  {
-    if (!origin || !destination) return;
+  useEffect(() =>
+    //     {
+    //   if (mapRef.current && destination?.location) {
+    //     mapRef.current.animateToRegion({
+    //       // latitude: origin.location.latitude,
+    //       // longitude: origin.location.longitude,
+    //       latitude: destination.location.latitude,
+    //       longitude: destination.location.longitude,
+    //       latitudeDelta: 0.005,
+    //       longitudeDelta: 0.005,
+    //     });
+    //   }
+    // }, [destination]
+    {
+      if (!origin || !destination) return;
 
-    setTimeout(() => {
-      mapRef.current.fitToSuppliedMarkers(['Origin', 'Destination'], {
-        edgePadding: { top: 50, right: 50, left: 50, bottom: 50 },
-        animated: true,
-      });
-    }, 500);
-  }, [destination]
-);
+      setTimeout(() => {
+        mapRef.current.fitToSuppliedMarkers(["Origin", "Destination"], {
+          edgePadding: { top: 50, right: 50, left: 50, bottom: 50 },
+          animated: true,
+        });
+      }, 500);
+    }, [destination]);
 
   useEffect(() => {
     const fetchMusollahs = async () => {
       try {
-        const colRef = collection(FIRESTORE_DB, 'musollahs');
+        const colRef = collection(FIRESTORE_DB, "musollahs");
         const snapshot = await getDocs(colRef);
-        const data = snapshot.docs.map(doc => doc.data());
+        const data = snapshot.docs.map((doc) => doc.data());
         // console.log(data);
         setMusollahs(data);
       } catch (error) {
-        console.error('Error fetching musollahs:', error);
+        console.error("Error fetching musollahs:", error);
       }
     };
 
@@ -100,14 +99,14 @@ const Map = () => {
   }, []);
 
   return (
-    <MapView 
-      provider='google'
+    <MapView
+      provider={PROVIDER_GOOGLE}
       style={styles.map}
       initialRegion={{
-          latitude: origin.location.latitude,
-          longitude: origin.location.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005,
+        latitude: origin.location.latitude,
+        longitude: origin.location.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
       ref={mapRef}
       showsMyLocationButton={true}
@@ -115,88 +114,95 @@ const Map = () => {
       followsUserLocation={true}
       showsCompass={true}
       onMapReady={() => {
-        mapRef.current.animateToRegion([{
-          latitude: 1.2968749382461264, 
-          longitude: 103.77642571509065,
-          }]
-        )}
-    }
-    >
-    {origin && destination && (
-      <MapViewDirections
-      apikey={GOOGLE_MAPS_APIKEY}
-      strokeWidth={3}
-      origin={{
-        latitude: origin.location.latitude,
-        longitude: origin.location.longitude,
-    }}
-      destination={{
-        latitude: destination.location.latitude,
-        longitude: destination.location.longitude,
-    }}
-      onError={(errorMessage) => {
-        // console.log('MapViewDirections Error: ', errorMessage);
-        // console.log('Origin: ', origin.location);
-        // console.log('Destination: ', destination.location);
+        mapRef.current.animateToRegion([
+          {
+            latitude: 1.2968749382461264,
+            longitude: 103.77642571509065,
+          },
+        ]);
       }}
-      />
-    )}
+    >
+      {origin && destination && (
+        <MapViewDirections
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          origin={{
+            latitude: origin.location.latitude,
+            longitude: origin.location.longitude,
+          }}
+          destination={{
+            latitude: destination.location.latitude,
+            longitude: destination.location.longitude,
+          }}
+          onError={(errorMessage) => {
+            // console.log('MapViewDirections Error: ', errorMessage);
+            // console.log('Origin: ', origin.location);
+            // console.log('Destination: ', destination.location);
+          }}
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
-              latitude: origin.location.latitude,
-              longitude: origin.location.longitude,
-            }
-          }
-          title='Origin'
-          identifier='Origin'
-        />
+            latitude: origin.location.latitude,
+            longitude: origin.location.longitude,
+          }}
+          // centerOffset={{ x: -10, y: -60 }}
+          title="Origin"
+          identifier="Origin"
+          image={require('../assets/images/Man.png')}
+        >
+          {/* <Image
+            style={{height: 50, width: 50}}
+            source={require('../assets/images/Man.png')}
+          /> */}
+        </Marker>
       )}
 
       {destination?.location && (
         <Marker
-          coordinate={
-            {
-              latitude: destination.location.latitude,
-              longitude: destination.location.longitude,
-            }
-          }
-          title='Destination'
-          identifier='Destination'
+          coordinate={{
+            latitude: destination.location.latitude,
+            longitude: destination.location.longitude,
+          }}
+          title="Destination"
+          identifier="Destination"
+          image={require('../assets/images/greenFlag_resized.png')}
         />
       )}
 
       {musollahs.map((musollah) => (
         <Marker
           key={musollah.name}
-          coordinate={
-            {
-              latitude: musollah.latitude,
-              longitude: musollah.longitude
-            }
-          }
+          coordinate={{
+            latitude: musollah.latitude,
+            longitude: musollah.longitude,
+          }}
           title={musollah.name}
         >
-          <Callout style={{height: 200, width: 200, padding: 15, flex: 1}}
-          onPress={() => {
-            navigation.navigate('MapInfoScreen', {musollah});
-            console.log(musollah.image);
-          }}
+          <Callout
+            className='p-14 max-w-xs'
+            onPress={() => {
+              navigation.navigate("MapInfoScreen", { musollah });
+              // console.log(musollah.image);
+            }}
           >
-            <View className='items-center justify-between align-middle'>
+            <View className="flex-wrap items-center justify-center p-10">
               <View>
-                <Text>{musollah.name}</Text>
+                <Text className='mb-1'>{musollah.name}</Text>
               </View>
               <View>
-                <Text className='w-full h-full'>
-                  <Image 
-                  source={{uri: musollah.image}} 
+                {/* <Image 
+                  className="w-full h-full"
+                  source={{ uri: musollah.image }}
                   // source={require("../assets/images/Masjid_icon.jpg")}
-                  onError={(e) => console.log(`Image load error: ${e.nativeEvent.error}`)}
-                  style={{height: 150, width:150}}
-                  resizeMode='cover'
-                  />
-                </Text> 
+                  onError={(e) =>
+                    console.log(`Image load error: ${e.nativeEvent.error}`)
+                  }
+                  style={{ height: 150, width: 150 }}
+                  resizeMode="cover"
+                /> */}
+                <Text>Click for more info!</Text>
               </View>
               {/* <TouchableOpacity style={{color: 'blue'}}
                   onPress={() => Linking.openURL(musollah.link)}>
@@ -207,8 +213,8 @@ const Map = () => {
         </Marker>
       ))}
     </MapView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   map: {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,22 @@
-import { Image, View, TextInput, TouchableOpacity, Text, ImageBackground } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { useState } from 'react';
-import { FIREBASE_AUTH } from '../FirebaseConfig';
-import { signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { ActivityIndicator } from 'react-native';
+import {
+  Image,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  ImageBackground,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import { signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ActivityIndicator } from "react-native";
 
 const LoginScreen = () => {
   const navigation = useNavigation();
-  const [Email, setEmail] = useState('');
-  const [Password, setPassword] = useState(''); 
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
@@ -27,53 +34,55 @@ const LoginScreen = () => {
   };
 
   return (
-    <View className='flex-1'> 
-      <ImageBackground 
-      source={require('../assets/images/bg-green.jpg')} 
-      // source={{uri: 'https://cde.nus.edu.sg/wp-content/uploads/2023/08/NUS_SDE1_Finbarr_Fallon_198.png'}}
-      resizeMode='cover'
-      className='w-full h-full absolute'/>
-      <View className='flex space-y-4 mx-4 items-center'>
-        <View className='w-full mt-80 rounded-2xl items-center bg-gray-100'>
-          <TextInput 
-            placeholder='Email' 
-            placeholderTextColor={'gray'} 
-            autoCapitalize='none'
+    <View className="flex-1">
+      <ImageBackground
+        source={require("../assets/images/bg-green.jpg")}
+        // source={{uri: 'https://cde.nus.edu.sg/wp-content/uploads/2023/08/NUS_SDE1_Finbarr_Fallon_198.png'}}
+        resizeMode="cover"
+        className="w-full h-full absolute"
+      />
+      <View className="flex space-y-4 mx-4 items-center">
+        <View className="w-full mt-80 rounded-2xl items-center bg-gray-100 pt-2 pb-2">
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor={"gray"}
+            autoCapitalize="none"
             value={Email}
-            onChangeText={text => setEmail(text)}
+            onChangeText={(text) => setEmail(text)}
           />
         </View>
-        <View className='w-full rounded-2xl items-center bg-gray-100'>
-          <TextInput 
-            placeholder='Password' 
-            placeholderTextColor={'gray'} 
-            autoCapitalize='none'
+        <View className="w-full rounded-2xl items-center bg-gray-100 pt-2 pb-2">
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={"gray"}
+            autoCapitalize="none"
             value={Password}
-            onChangeText={text => setPassword(text)}
+            onChangeText={(text) => setPassword(text)}
             secureTextEntry={true}
           />
         </View>
-        <View className='flex-row'></View>
+        <View className="flex-row"></View>
         {loading ? (
           <ActivityIndicator size="Large" color="#000ff" />
         ) : (
-          <> 
-            <TouchableOpacity 
-              className='w-full rounded-2xl items-center bg-green-200 p-2'
-              onPress={Login}>
-                <Text>Login</Text>
+          <>
+            <TouchableOpacity
+              className="w-full rounded-2xl items-center bg-green-200 p-2"
+              onPress={Login}
+            >
+              <Text>Login</Text>
             </TouchableOpacity>
           </>
         )}
-        <View className='flex-row'>
+        <View className="flex-row">
           <Text>Don't have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.push('SignupScreen')}>
-            <Text className='text-blue-500 underline'> Sign Up</Text>
+          <TouchableOpacity onPress={() => navigation.push("SignupScreen")}>
+            <Text className="text-blue-500 underline"> Sign Up</Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default LoginScreen;
